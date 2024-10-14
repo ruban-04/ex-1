a9ed9bfc80be85c0a4d2b2271aa0359c18a29a05
ruban <kavitharuban3002@gmail.com>
2024-10-14 16:52:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var result=50;
+if(result>60)
+
+    {
+        console.log("pass")
+    }
+    else
+    {
+        console.log("fail")
+    }
+
+
+    var score=60
+    if(score>90)
+    {
+        console.log("nivin")
+    }
+    else if(score>=60)
+    {
+        console.log("samar")
+    }
+    else if(score>10)
+    {
+        console.log("prakash")
+    } 
+    else{
+        console.log("ruban")
+    }
+    
+
+    var a=20;
+    ++a;
+    console.log(a);
+  
+    var a=20;
+    b=a++;
+    console.log(b);
+    
+    var a=60;
+    b=--a;
+    console.log(b);
